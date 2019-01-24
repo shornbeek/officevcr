976754f19c61fd15a32ab162a4b1e09341caa158
Stephen Hornbeek <hornbeeks@gmail.com>
2019-01-23 22:09:48 -0800

    email valiadation on home page

--- public/js/main.js
+++ public/js/main.js
@@ -63,4 +63,29 @@ url.searchParams.append('x', newUser.random);
     }
 
 // Retrieve
-document.getElementById("result").innerHTML = localStorage.getItem("id");
+// document.getElementById("result").innerHTML = localStorage.getItem("id");
+
+function validateEmail(email) {
+    var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(email);
+  }
+  
+  function validate() {
+    var $result = $("#result");
+    var email = $("#email").val();
+    $result.text("");
+  
+    if (validateEmail(email)) {
+    //   $result.text("Email " + email + " is valid :)");
+    //   $result.css("color", "green");
+      window.location.href = "/conference"
+
+    } else {
+      $result.text("Email " + email + " is not valid. Try Again");
+      $result.css("color", "red");
+      $result.css("margin-top", "22px");
+    }
+    return false;
+  }
+  
+  $(".validate").bind("click", validate);
